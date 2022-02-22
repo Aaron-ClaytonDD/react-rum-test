4e759538bf08054719d0d8ee248299ee888a917c
Aaron-ClaytonDD <aaron.clayton@datadoghq.com>
2022-02-22 17:38:23 +0000

    adding RUM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { datadogRum } from '@datadog/browser-rum';
+    
+datadogRum.init({
+    applicationId: '6972b043-3f82-43e8-ada8-07ffdf48123d',
+    clientToken: 'pub3333f6d81339a88d9e43af5a9dd3afad',
+    site: 'datadoghq.com',
+    service:'rum-test',
+    env:'aarons-env',
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel: 'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
 
 function App() {
   return (
